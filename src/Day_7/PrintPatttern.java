d8e76f1b243c02325483c31233c4699565061d36
Snehashish Panigrahi <panigrahisnehasis@gmail.com>
2019-02-08 14:58:48 +0530

    PrintPattern variant of Diamond Patterns solved.

---
+++ src/Day_7/PrintPatttern.java
@@ -0,0 +1,44 @@
+package Day_7;
+
+import java.util.Scanner;
+
+public class PrintPatttern {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int testCases = sc.nextInt();
+        int[] inputArr = new int[testCases];
+        for (int i = 0; i < testCases; i++) {
+            inputArr[i] = sc.nextInt();
+        }
+
+        for (int input : inputArr) {
+            int size = input * 2 - 1;
+            int[][] pattern = new int[size][size];
+            for (int i = 0; i < size; i++) {
+                pattern[0][i] = input;
+            }
+            for (int i = 1; i <= size / 2; i++) {
+                for (int j = 0; j < size; j++) {
+                    if (j >= i && j < size - i) {
+                        pattern[i][j] = input - i;
+                    } else pattern[i][j] = pattern[i - 1][j];
+                }
+            }
+
+            for (int i = 0; i < input; i++) {
+                for (int j = 0; j < size; j++) {
+                    System.out.print(pattern[i][j] + " ");
+                }
+                System.out.println();
+            }
+
+            for (int i = size / 2 - 1; i >= 0; i--) {
+                for (int j = 0; j < size; j++) {
+                    System.out.print(pattern[i][j] + " ");
+                }
+                System.out.println();
+            }
+        }
+
+    }
+}
